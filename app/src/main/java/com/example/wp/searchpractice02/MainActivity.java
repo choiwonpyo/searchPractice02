f16259b75f723ba67e846f43fdda9eca880dbf33
JK <0j77750@hanmail.net>
2016-05-08 20:46:54 +0900

    fefkwlfj

--- app/src/main/java/com/example/wp/searchpractice02/MainActivity.java
+++ app/src/main/java/com/example/wp/searchpractice02/MainActivity.java
@@ -23,7 +23,7 @@ import android.widget.Toast;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-
+//im' jnkgu
 //hello baby this is practice
 //fucking git
 public class MainActivity extends AppCompatActivity {
